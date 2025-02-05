b3bf61046104868ded17337f78b7709d9834288e
rooban33 <shajithgunasekaran@gmail.com>
2025-02-05 12:49:56 +0530

    safari

--- melinia/src/Components/workshop.js
+++ melinia/src/Components/workshop.js
@@ -83,7 +83,6 @@ function Workshop() {
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-between",
-              justifyItems:"center",
               alignItems: "center",
               textAlign: "center",
               transition: "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
